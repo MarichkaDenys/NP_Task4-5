fd5ef9b41d5448666eb2eb03c1900149f6504a32
MarichkaDenys <99760032+MarichkaDenys@users.noreply.github.com>
2022-12-01 18:04:01 +0200

    Update Logger.h

--- Bookshop/Logger/Logger.h
+++ Bookshop/Logger/Logger.h
@@ -1,1 +1,36 @@
+#pragma once
 
+#include <fstream>
+using namespace std;
+
+class Logger
+{
+	Logger() {};
+
+	static Logger* instance;
+
+public:
+	Logger(Logger&) = delete;
+
+	Logger& operator = (Logger&) = delete;
+
+	void LogError(string error)
+	{
+		ofstream fout("C:\\Users\\МаРіЧкА\\source\\repos\\Bookshop4\\Bookshop4\\errors.txt");
+		fout << error << endl;
+
+		fout.close();
+	}
+
+	static Logger* GetInstance()
+	{
+		if (instance == nullptr)
+		{
+			instance = new Logger();
+		}
+
+		return instance;
+	}
+};
+
+Logger* Logger::instance = nullptr;
